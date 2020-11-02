87ec79c7b46ddc006df4766cb8d98541501deeae
Nicolás Escudero Rivera <nescuderor@unal.edu.co>
2020-11-02 09:35:19 -0500

    Update

---
+++ Marketing analytics data based/Evaluación de factores.R
@@ -0,0 +1,93 @@
+### Cargar la base de datos
+library(haven)
+PRICING <- read_sav("C:/Users/nicol/OneDrive/Escritorio/PRICING.sav")
+View(PRICING)
+
+#Instalación de paquetes
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("labelled")
+install.packages("magrittr")
+install.packages("reshape")
+
+### Modelamiento
+#Exploración de  la base de datos
+library(tidyverse)
+
+PRICING %>% glimpse
+PRICING %>% str
+PRICING %>% View
+PRICING %>% names
+
+#Exploración de etiquetas
+library(labelled)
+
+var_label(PRICING)
+var_label(PRICING$VAR00013)
+
+#Renombrando variables
+data_fr <- PRICING %>% 
+  transmute(
+    Alta_Calidad_U1 = as.double(VAR00010),
+    Alta_Calidad_U2 = as.double(VAR00011),
+    Alta_Calidad_U3 = as.double(VAR00012),
+    P.Valor_U1 = as.double(VAR00013),
+    P.Valor_U2 = as.double(VAR00014),
+    P.Valor_U3 = as.double(VAR00015),
+  )
+new_dt <- data.frame(data_fr)
+
+##Tratamiento de NAs y transformaciones iniciales
+tail(new_dt)
+new_dt <- new_dt[1:109,]
+
+#Indexación por medio del remplazamiento por medias ante la existencia de NAs y NaNs
+NAT1 <- mean(new_dt$Alta_Calidad_U1[!is.na(new_dt$Alta_Calidad_U1)]) #Tratamiento de NAs
+NAT2 <- mean(new_dt$Alta_Calidad_U2[!is.na(new_dt$Alta_Calidad_U2)]) #Tratamiento de NAs
+NAT3 <- mean(new_dt$Alta_Calidad_U3[!is.na(new_dt$Alta_Calidad_U3)]) #Tratamiento de NAs
+NAT4 <- mean(new_dt$P.Valor_U1[!is.na(new_dt$P.Valor_U1)]) #Tratamiento de NAs
+NAT5 <- mean(new_dt$P.Valor_U2[!is.na(new_dt$P.Valor_U2)]) #Tratamiento de NAs
+NAT6 <- mean(new_dt$P.Valor_U3[!is.na(new_dt$P.Valor_U3)]) #Tratamiento de NAs
+
+new_dt$Alta_Calidad_U1[is.na(new_dt$Alta_Calidad_U1)] <- NAT1
+new_dt$Alta_Calidad_U2[is.na(new_dt$Alta_Calidad_U2)] <- NAT2
+new_dt$Alta_Calidad_U3[is.na(new_dt$Alta_Calidad_U3)] <- NAT3
+new_dt$P.Valor_U1[is.na(new_dt$P.Valor_U1)] <- NAT4
+new_dt$P.Valor_U2[is.na(new_dt$P.Valor_U2)] <- NAT5
+new_dt$P.Valor_U3[is.na(new_dt$P.Valor_U3)] <- NAT6
+
+
+#Determinación de medias
+AC_U1 <- mean(new_dt$Alta_Calidad_U1)
+AC_U2 <- mean(new_dt$Alta_Calidad_U2)
+AC_U3 <- mean(new_dt$Alta_Calidad_U3)
+PV_U1 <- mean(new_dt$P.Valor_U1)
+PV_U2 <- mean(new_dt$P.Valor_U2)
+PV_U3 <- mean(new_dt$P.Valor_U3)
+
+#Modelamiento y graficación
+library(reshape)
+final_dt <- data.frame(c(AC_U1, AC_U2, AC_U3), c(PV_U1, PV_U2, PV_U3), 
+                       row.names = c("Universidad 1", "Universidad 2", "Universidad 3"))
+final_dt <- rename(final_dt, c(c.AC_U1..AC_U2..AC_U3.="Alta_Calidad", 
+                               c.PV_U1..PV_U2..PV_U3.="P.Valor"))
+
+final_dt
+
+#Graficación
+library(ggplot2)
+Dispersion <- ggplot(final_dt, aes(x = P.Valor, y = Alta_Calidad)) +
+  geom_point() + geom_point(aes(colour = row.names(final_dt))) +
+  scale_x_continuous(limits = c(0,10),
+                     expand = c(0,0),
+                     breaks = seq(0, 10, 1)) +
+  scale_y_continuous(limits = c(0,10),
+                     expand = c(0,0),
+                     breaks = seq(0, 10, 1)) +
+  geom_segment(aes(x = 0, 
+                   xend = 10,
+                   y = 0,
+                   yend = 10)) +
+  geom_text(aes(label=row.names(final_dt)),cex = 2, nudge_x = -0.4, nudge_y = 0.1 )
+  
+Dispersion
---
+++ Marketing analytics data based/Van_Westendorp_model.R
@@ -0,0 +1,100 @@
+### Cargar la base de datos
+library(haven)
+PRICING <- read_sav("C:/Users/nicol/OneDrive/Escritorio/PRICING.sav")
+View(PRICING)
+
+#Instalación de paquetes
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("labelled")
+install.packages("magrittr")
+
+## Modelamiento
+#Exploración de  la base de datos
+library(tidyverse)
+
+PRICING %>% glimpse
+PRICING %>% str
+PRICING %>% View
+PRICING %>% names
+
+#Exploración de etiquetas
+library(labelled)
+
+var_label(PRICING)
+var_label(PRICING$VAR00013)
+
+#Renombrando variables
+data_fr <- PRICING %>% 
+  transmute(
+    Too_cheap = as_factor(VAR00024),
+    Cheap = as_factor(VAR00025),
+    Expensive = as_factor(VAR00026),
+    Too_expensive = as_factor(VAR00027),
+  )
+
+View(data_fr)
+data.class(data_fr)
+
+#Tratamientos de NAs
+Too_cheap <- data_fr$Too_cheap[!is.na(data_fr$Too_cheap)]
+Cheap <- data_fr$Cheap[!is.na(data_fr$Cheap)]
+Expensive <- data_fr$Expensive[!is.na(data_fr$Expensive)]
+Too_expensive <- data_fr$Too_expensive[!is.na(data_fr$Too_expensive)]
+
+new_datafr <- data.frame(Too_cheap, Cheap, Expensive, Too_expensive)
+View(new_datafr)
+
+#Conversión a numeros
+llave <- c(
+  "1 MILLÓN" = 1000000,
+  "2 MILLONES" = 2000000,
+  "3 MILLONES" = 3000000,
+  "4 MILLONES" = 4000000,
+  "5 MILLONES" = 5000000,
+  "6 MILLONES" = 6000000,
+  "7 MILLONES" = 7000000,
+  "8 MILLONES" = 8000000,
+  "9 MILLONES" = 9000000,
+  "10 MILLONES" = 10000000,
+  "11 MILLONES" = 11000000,
+  "12 MILLONES" = 12000000,
+  "13 MILLONES" = 13000000,
+  "14 MILLONES" = 14000000,
+  "15 MILLONES" = 15000000
+)
+
+New_pricing.df <- new_datafr %>%
+  transmute(
+    Too_cheap = llave[as.character(Too_cheap)],
+    Cheap = llave[as.character(Cheap)],
+    Expensive = llave[as.character(Expensive)],
+    Too_expensive = llave[as.character(Too_expensive)],
+  )
+View(New_pricing.df)
+
+#Frecuencias <- Establecimiento de las frecuencias acumuladas
+Rangos <-  seq(0, 15000000, 1000000)
+
+library(magrittr)
+Pricing_Model <- New_pricing.df %$%
+  tibble(
+    Rangos = Rangos,
+    Too_cheap = outer(Rangos, Too_cheap, "<=") %>% rowMeans(),
+    Cheap = outer(Rangos, Cheap, "<=") %>% rowMeans(),
+    Expensive = outer(Rangos, Expensive, ">=") %>% rowMeans(),
+    Too_expensive = outer(Rangos, Too_expensive, ">=") %>% rowMeans(),
+  )
+
+#Desarrollo final del modelo
+Van_Westendorp_model <- Pricing_Model %>%
+  gather(key = "Variable",value = "Valor", Too_cheap:Too_expensive)
+
+#Proyección final
+Van_Westendorp_graph <- Van_Westendorp_model %>%
+  ggplot + aes(x = Rangos, y = Valor, colour = Variable) + geom_line() + 
+  scale_y_continuous(breaks = seq(0, 1, 0.1), labels = scales::percent) + 
+  scale_x_continuous(breaks = seq(0,15000000,1000000), labels = scales::comma) +  
+  theme(axis.text.x = element_text(angle = 270, hjust = 1))
+
+Van_Westendorp_graph
